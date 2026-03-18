e76225713142326e36e44fec9e38494f007a427a
AlbertoPazToja <albertopaztoja@gmail.com~>
2026-03-18 10:01:59 +0100

    ejercicio 4 terminado

--- e4/src/main/java/com/example/Coche.java
+++ e4/src/main/java/com/example/Coche.java
@@ -1,21 +1,42 @@
 package com.example;
 
+/**
+ * Se crea la clase Coche con atributos marca, modelo (texto) y velocidad
+ * (numero entero)
+ */
+
 public class Coche {
 
     private String marca;
     private String modelo;
     private int velocidad;
 
+    /**
+     * Método constructor para crear el objeto coche
+     * 
+     * @param marca  El nombre de la marca del coche
+     * @param modelo El nombre del modelo del coche
+     */
     public Coche(String marca, String modelo) {
         this.marca = marca;
         this.modelo = modelo;
         this.velocidad = 0;
     }
 
+    /**
+     * Método que añade un valor a la variable velocidad
+     * 
+     * @param incremento El valor que le queremos añadir a la variable velocidad
+     */
     public void acelerar(int incremento) {
         velocidad += incremento;
     }
 
+    /**
+     * Método que reduce el valor de velocidad
+     * 
+     * @param decremento El valor que queremos reducir de velodidad
+     */
     public void frenar(int decremento) {
         velocidad -= decremento;
         if (velocidad < 0) {
@@ -23,6 +44,11 @@ public class Coche {
         }
     }
 
+    /**
+     * Método que devuelve el valor de la variable velodidad
+     * 
+     * @return el valor de velodidad
+     */
     public int getVelocidad() {
         return velocidad;
     }
--- e4/src/main/java/com/example/Libro.java
+++ e4/src/main/java/com/example/Libro.java
@@ -1,25 +1,51 @@
 package com.example;
 
+/**
+ * Clase libro con los atributos titulo, autor (cadenas de texto) y paginas
+ * (numero entero).
+ */
 public class Libro {
 
     private String titulo;
     private String autor;
     private int paginas;
 
+    /**
+     * Método constructor para crear el objeto libro.
+     * 
+     * @param titulo  el nombre de la obra.
+     * @param autor   el nombre del autor.
+     * @param paginas el número de páginas.
+     */
     public Libro(String titulo, String autor, int paginas) {
         this.titulo = titulo;
         this.autor = autor;
         this.paginas = paginas;
     }
 
+    /**
+     * Método que devuelve el número de páginas si son más de 300.
+     * 
+     * @return el valor de páginas si son más de 300.
+     */
     public boolean esLargo() {
         return paginas > 300;
     }
 
+    /**
+     * Método que muestra el título del libro.
+     * 
+     * @return el valor del atributo libro.
+     */
     public String getTitulo() {
         return titulo;
     }
 
+    /**
+     * Método que muestra el autor del libro.
+     * 
+     * @return el valor del atributo autor.
+     */
     public String getAutor() {
         return autor;
     }
